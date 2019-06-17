10db26bdf91d18ee84957347fedcd850dc27696b
Reika <reikasminecraft@gmail.com>
2019-06-17 12:12:45 -0400

    Update 2

---
+++ control.lua
@@ -0,0 +1,54 @@
+require "entitytracker"
+--require "eventhandling"
+
+script.on_configuration_changed(function()
+	
+end)
+
+script.on_init(function()
+
+end)
+
+script.on_load(function()
+
+end)
+
+function onCommand(event)
+
+end
+
+function onEntityRotated(event)
+
+end
+
+function onEntityAdded(event)
+
+end
+
+function onEntityRemoved(event)
+
+end
+
+function onEntityDied(event)
+	onEntityRemoved(event)
+end
+
+function onGameTick(event)	
+	local tick = event.tick
+end
+
+script.on_event(defines.events.on_console_command, onCommand)
+
+script.on_event(defines.events.on_entity_died, onEntityDied)
+script.on_event(defines.events.on_pre_player_mined_item, onEntityRemoved)
+script.on_event(defines.events.on_robot_pre_mined, onEntityRemoved)
+script.on_event(defines.events.script_raised_destroy, onEntityRemoved)
+
+script.on_event(defines.events.on_built_entity, onEntityAdded)
+script.on_event(defines.events.on_robot_built_entity, onEntityAdded)
+
+script.on_event(defines.events.on_player_rotated_entity, onEntityRotated)
+
+--script.on_event(defines.events.on_entity_damaged, onEntityDamaged)
+
+script.on_event(defines.events.on_tick, onGameTick)
